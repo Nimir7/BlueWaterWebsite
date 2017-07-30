5ea151f3a8915b22b44c4c2ee2d33f4b2e9fb620
Nimir7 <mustafababo@gmail.com>
2017-07-30 20:40:01 +0300

    New Web Updates

---
+++ BluWaterSystems/Scripts/scrolling.js
@@ -0,0 +1,50 @@
+﻿
+
+$(function () {
+    $(window).on("load resize", function () {
+        $(".fill-screen").css("height", window.innerHeight);
+    });
+
+    // add Bootstrap's scrollspy
+    $('body').scrollspy({
+        target: '.navbar'
+    });
+
+     //smooth scrolling
+    $('nav a, .down-button a').bind('click', function () {
+        $('html, body').stop().animate({
+            scrollTop: $($(this).attr('href')).offset().top -10
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // parallax scrollig with stellar.js
+    //$(window).stellar();
+
+    // initialize WOW for element animation
+    new WOW().init();
+
+    // initialize nanoGallary
+    //$(document).ready(function () {
+    //    $("#nanoGallery3").nanoGallery();
+    //});
+
+    // test
+    //$('a[href*="#"]:not([href="#"])').click(function () {
+    //    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    //        var target = $(this.hash);
+    //        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    //        if (target.length) {
+    //            $('html, body').animate({
+    //                scrollTop: target.offset().top - 83
+    //            }, 1000);
+    //            return false;
+    //        }
+    //    }
+    //});
+
+    $(document).on("pageload", function () {
+        alert("pageload event fired!");
+    });
+
+});
